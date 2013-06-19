2e865fe663adc06d7130ca6e20a8735fe42de78a
ovasylev <ovasylev@me.com>
2013-06-19 15:21:03 -0400

    Added chap6.py and chap7.py files with code.

--- chap6.py
+++ chap6.py
@@ -1,9 +1,23 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Olha Vasylevska
 
 
 # Ex. 6.6
+def is_palindrome(word):
+	length = len(word)
+	for i in range (length/2):
+		if word[i]!=word[length-1-i]:
+			return False
+	return True
 
-
+print is_palindrome("noon")
 
 # Ex 6.8
+def gcd(a, b):
+	if b==0:
+		return a
+	else:
+		r = a % b
+		return gcd(b, r)
+
+print gcd(8, 12)
--- chap7.py
+++ chap7.py
@@ -1,9 +1,35 @@
 # Enter your answrs for chapter 7 here
-# Name:
+# Name:Olha Vasylevska
 
 
 # Ex. 7.5
+import math
 
+
+def factorial(n):
+	if n==0:
+		return 1
+	else:
+		return n*factorial(n-1)
+
+
+def estimate_pi():
+	sum = 0
+	k = 0
+	koef = 2 * math.sqrt(2) / 9801
+	while True:
+		a = factorial(4*k) * (1103 + 26390*k)
+		b = factorial(k)**4 * 396**(4*k)
+		formula = koef * a / b
+		sum += formula
+		
+		if abs(formula) < 1e-15: break
+		k += 1
+
+	return 1 / sum
+
+print estimate_pi()
 
 
 # How many iterations does it take to converge?
+2 iterations
